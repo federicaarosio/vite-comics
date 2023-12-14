25fe6370782aec5446d72508e0a787b05ab70d43
federicaarosio <arosio.fede@gmail.com>
2023-12-14 16:29:05 +0100

    AppHeader: added logo and list

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,24 @@
 <template lang="">
     <header>
-        <h1>
-            Header
-        </h1>
+        <section class="container">
+            <section class="logo">
+                <img src="../assets/img/dc-logo.png" alt="DC logo">
+            </section>
+            <nav class="navigation">
+                <ul>
+                    <li>
+                        Characters
+                    </li>
+                    <li>
+                        Comics
+                    </li>
+                    <li>
+                        Movies
+                    </li>
+    
+                </ul>
+            </nav>
+        </section>
     </header>
 </template>
 <script>
@@ -14,6 +30,31 @@ export default {
 @use "../styles/partials/variables" as *;
 @use "../styles/partials/mixins" as *;
 
+header {
+    background-color: $header-bg-color;
+    margin-top: 15px;
+}
 
+section.container {
+    @include flex(row, space-between, center);
+    width: 1100px;
+    margin: 0 auto;
+    padding: 1rem;
+    font-size: .8rem;
+    text-transform: uppercase;
+}
+
+section.logo > img {
+    width: 70px;
+}
+
+nav.navigation > ul {
+    @include flex(row, center, center);
+    list-style: none;
+
+    li {
+        margin-right: 1rem;
+    }
+}
 
 </style>
