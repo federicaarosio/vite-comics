ecf44b210a83e027ec48612b44dab5cee17ff843
federicaarosio <arosio.fede@gmail.com>
2023-12-15 15:23:12 +0100

    added Comic Element

---
+++ src/components/ComicElement.vue
@@ -0,0 +1,21 @@
+<template>
+    <article>
+        <h2>
+            Comic Element
+        </h2>
+    </article>
+</template>
+<script>
+export default {
+    name: 'ComicElement',
+}
+</script>
+<style lang="scss">
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
+
+    article {
+        background-color: white;
+        width: calc(100% / 6);
+    }
+</style>
